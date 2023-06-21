7627fb34665453eae81325fcaf29837ccab72db0
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-06-21 06:33:17 +0000

    added token decimals to model to sync with python api

--- src/models/token.ts
+++ src/models/token.ts
@@ -4,6 +4,7 @@ export interface IToken {
     token_name: mongoose.Schema.Types.String
     token_symbol: mongoose.Schema.Types.String
     token_spl_addr: mongoose.Schema.Types.String
+    token_decimals: mongoose.Schema.Types.Number
     token_image_url: mongoose.Schema.Types.String
     token_price_feed: mongoose.Schema.Types.String
     token_ltp: mongoose.Schema.Types.Number
@@ -28,6 +29,9 @@ export const TokenSchema = new mongoose.Schema<IToken>(
             type: mongoose.Schema.Types.String,
             required: true,
             unique: true,
+        },
+        token_decimals: {
+            type:mongoose.Schema.Types.Number
         },
         token_image_url: {
             type: mongoose.Schema.Types.String,
